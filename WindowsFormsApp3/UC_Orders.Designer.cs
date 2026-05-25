ac011c73a437cb52edb884ae1fecbff71be258dd
Ashaz Mohammed <2014446@schoolsnet.act.edu.au>
2026-05-25 10:35:26 +1000

    Login added 2

--- WindowsFormsApp3/UC_Orders.Designer.cs
+++ WindowsFormsApp3/UC_Orders.Designer.cs
@@ -46,10 +46,9 @@
             // 
             // btnLoadOrder
             // 
-            this.btnLoadOrder.Location = new System.Drawing.Point(1027, 491);
-            this.btnLoadOrder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLoadOrder.Location = new System.Drawing.Point(770, 399);
             this.btnLoadOrder.Name = "btnLoadOrder";
-            this.btnLoadOrder.Size = new System.Drawing.Size(100, 28);
+            this.btnLoadOrder.Size = new System.Drawing.Size(75, 23);
             this.btnLoadOrder.TabIndex = 23;
             this.btnLoadOrder.Text = "Load Order";
             this.btnLoadOrder.UseVisualStyleBackColor = true;
@@ -57,10 +56,9 @@
             // 
             // btnSaveOrder
             // 
-            this.btnSaveOrder.Location = new System.Drawing.Point(745, 491);
-            this.btnSaveOrder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSaveOrder.Location = new System.Drawing.Point(559, 399);
             this.btnSaveOrder.Name = "btnSaveOrder";
-            this.btnSaveOrder.Size = new System.Drawing.Size(100, 28);
+            this.btnSaveOrder.Size = new System.Drawing.Size(75, 23);
             this.btnSaveOrder.TabIndex = 22;
             this.btnSaveOrder.Text = "Save Order";
             this.btnSaveOrder.UseVisualStyleBackColor = true;
@@ -69,49 +67,44 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(993, 201);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(745, 163);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 16);
+            this.label2.Size = new System.Drawing.Size(89, 13);
             this.label2.TabIndex = 21;
             this.label2.Text = "Order Description";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(993, 130);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(745, 106);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.Size = new System.Drawing.Size(64, 13);
             this.label1.TabIndex = 20;
             this.label1.Text = "Order Name";
             // 
             // btnCheckout
             // 
-            this.btnCheckout.Location = new System.Drawing.Point(525, 491);
-            this.btnCheckout.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCheckout.Location = new System.Drawing.Point(394, 399);
             this.btnCheckout.Name = "btnCheckout";
-            this.btnCheckout.Size = new System.Drawing.Size(100, 28);
+            this.btnCheckout.Size = new System.Drawing.Size(75, 23);
             this.btnCheckout.TabIndex = 19;
             this.btnCheckout.Text = "button3";
             this.btnCheckout.UseVisualStyleBackColor = true;
             // 
             // btnRemove
             // 
-            this.btnRemove.Location = new System.Drawing.Point(289, 491);
-            this.btnRemove.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRemove.Location = new System.Drawing.Point(217, 399);
             this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(177, 28);
+            this.btnRemove.Size = new System.Drawing.Size(133, 23);
             this.btnRemove.TabIndex = 18;
             this.btnRemove.Text = "Remove from Order";
             this.btnRemove.UseVisualStyleBackColor = true;
             // 
             // btnAddToOrder
             // 
-            this.btnAddToOrder.Location = new System.Drawing.Point(93, 491);
-            this.btnAddToOrder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddToOrder.Location = new System.Drawing.Point(70, 399);
             this.btnAddToOrder.Name = "btnAddToOrder";
-            this.btnAddToOrder.Size = new System.Drawing.Size(100, 28);
+            this.btnAddToOrder.Size = new System.Drawing.Size(75, 23);
             this.btnAddToOrder.TabIndex = 17;
             this.btnAddToOrder.Text = "Add to Order";
             this.btnAddToOrder.UseVisualStyleBackColor = true;
@@ -120,56 +113,50 @@
             // 
             this.lblTotal.AutoSize = true;
             this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotal.Location = new System.Drawing.Point(988, 71);
-            this.lblTotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotal.Location = new System.Drawing.Point(741, 58);
             this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(56, 25);
+            this.lblTotal.Size = new System.Drawing.Size(44, 20);
             this.lblTotal.TabIndex = 16;
             this.lblTotal.Text = "Total";
             // 
             // txtDescription
             // 
-            this.txtDescription.Location = new System.Drawing.Point(993, 220);
-            this.txtDescription.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDescription.Location = new System.Drawing.Point(745, 179);
             this.txtDescription.Name = "txtDescription";
-            this.txtDescription.Size = new System.Drawing.Size(132, 22);
+            this.txtDescription.Size = new System.Drawing.Size(100, 20);
             this.txtDescription.TabIndex = 15;
-            this.txtDescription.Text = "fddff";
             // 
             // txtOrderName
             // 
-            this.txtOrderName.Location = new System.Drawing.Point(993, 154);
-            this.txtOrderName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtOrderName.Location = new System.Drawing.Point(745, 125);
             this.txtOrderName.Name = "txtOrderName";
-            this.txtOrderName.Size = new System.Drawing.Size(132, 22);
+            this.txtOrderName.Size = new System.Drawing.Size(100, 20);
             this.txtOrderName.TabIndex = 14;
             // 
             // dgvCurrentOrder
             // 
             this.dgvCurrentOrder.AllowDrop = true;
             this.dgvCurrentOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCurrentOrder.Location = new System.Drawing.Point(289, 71);
-            this.dgvCurrentOrder.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvCurrentOrder.Location = new System.Drawing.Point(217, 58);
             this.dgvCurrentOrder.Name = "dgvCurrentOrder";
             this.dgvCurrentOrder.RowHeadersWidth = 51;
             this.dgvCurrentOrder.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvCurrentOrder.Size = new System.Drawing.Size(672, 399);
+            this.dgvCurrentOrder.Size = new System.Drawing.Size(504, 324);
             this.dgvCurrentOrder.TabIndex = 13;
             // 
             // dgvAvailable
             // 
             this.dgvAvailable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAvailable.Location = new System.Drawing.Point(48, 71);
-            this.dgvAvailable.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvAvailable.Location = new System.Drawing.Point(36, 58);
             this.dgvAvailable.Name = "dgvAvailable";
             this.dgvAvailable.RowHeadersWidth = 51;
             this.dgvAvailable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvAvailable.Size = new System.Drawing.Size(193, 399);
+            this.dgvAvailable.Size = new System.Drawing.Size(145, 324);
             this.dgvAvailable.TabIndex = 12;
             // 
             // UC_Orders
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.btnLoadOrder);
             this.Controls.Add(this.btnSaveOrder);
@@ -183,9 +170,8 @@
             this.Controls.Add(this.txtOrderName);
             this.Controls.Add(this.dgvCurrentOrder);
             this.Controls.Add(this.dgvAvailable);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "UC_Orders";
-            this.Size = new System.Drawing.Size(1187, 615);
+            this.Size = new System.Drawing.Size(890, 500);
             this.Load += new System.EventHandler(this.UC_Orders_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvCurrentOrder)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAvailable)).EndInit();
